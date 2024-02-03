a19b23079f2aa406271e30752c4a01e0f6f23f37
lamgnaoh <luonglamhoang2000@gmail.com>
2024-02-03 13:01:29 +0700

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [HomeComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent], // HomeComponent se la component dau tien duoc chay
 })
-export class AppModule { }
+export class AppModule {}
