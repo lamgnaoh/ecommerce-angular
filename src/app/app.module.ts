ea61bb5e57bbccdf077551ad60ad1deace5e9baa
lamgnaoh <luonglamhoang2000@gmail.com>
2024-02-15 01:19:01 +0700

    feat(login/register): add login register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { OrderComponent } from './order/order.component';
 import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -14,13 +16,17 @@ import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
     FooterComponent,
     OrderComponent,
     OrderConfirmComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [
     // HomeComponent,
     // OrderComponent,
-    OrderConfirmComponent,
+    // OrderConfirmComponent,
+    // LoginComponent,
+    RegisterComponent,
   ], // HomeComponent se la component dau tien duoc chay
 })
 export class AppModule {}
